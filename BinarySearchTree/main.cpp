da785f6f78f7e5d32613438a8c7eba93d6bfa0f9
wisdomG <kenpluver@hotmail.com>
2017-12-19 10:15:40 +0800

    Update main.cpp

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 
 int main() {
-    int arr[] = {1,2,3,4,5};
-    cout << binarySearch(arr, 5, 5) << endl;
+    int a[] = {1, 1, 1, 2, 2, 2, 2, 2, 4, 4, 5, 5, 5, 6, 6, 6};
+    int n = sizeof(a)/sizeof(int);
+    for( int i = 0 ; i <= 8 ; i ++ ){
+
+        int floorIndex = floor(a, n, i);
+        cout<<"the floor index of "<<i<<" is "<<floorIndex<<".";
+        if( floorIndex >= 0 && floorIndex < n )
+            cout<<"The value is "<<a[floorIndex]<<".";
+        cout<<endl;
+
+        int ceilIndex = ceil(a, sizeof(a)/sizeof(int), i);
+        cout<<"the ceil index of "<<i<<" is "<<ceilIndex<<".";
+        if( ceilIndex >= 0 && ceilIndex < n )
+            cout<<"The value is "<<a[ceilIndex]<<".";
+        cout<<endl;
+
+        cout<<endl;
+    }
     return 0;
 }
