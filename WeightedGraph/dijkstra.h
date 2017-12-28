d2ae2aa663bd8c8194119525158faec75a00cb30
WisdomCao <kenpluver@hotmail.com>
2017-12-28 14:19:19 +0800

    dijkstar algorithm for single source shortest path

---
+++ WeightedGraph/dijkstra.h
@@ -0,0 +1,16 @@
+#ifndef _DIJKSTRA_H_
+#define _DIJKSTRA_H_
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* Dijkstra algorithm
+ * requires that there is no negative weight int the graph
+ * the optimal time complexity is O(ElogV) when use minimum index heap
+ */
+class Dijkstra{
+
+};
+
+#endif
